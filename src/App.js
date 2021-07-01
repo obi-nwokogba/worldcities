5f508e402d157c1d3fb02207c2371499865e6d48
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-07-01 11:43:46 -0400

    City names being displayed

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import imagesArr from './imageData.js'
 
 export default function App() {
 
+  let currentHeader = "Cities of the World";
   const [bigPhoto, setBigPhoto] = useState(imagesArr[0]);
+  let [cityName, setCityName] = useState(imagesArr[0].city);
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
-  function handleClick(imageURL) {
+  function handleClick(imageURL,imageCaption) {
     setBigPhoto(imageURL);
+    setCityName(imageCaption);
   }
-
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key
-  // ALSO ASSIGN AN onClick EVENT THAT CALL THE HANDLE EVENT AND PASSES IT THE IMG URL
 
   return (
     <div className="App">
-      <h1>Cities Of The World</h1>
+      <h1>{cityName}</h1>
       <div id="wrapper">
 
         <img src={bigPhoto} id="bigimage" alt='bigImage' />
 
         <div id="thumbnails">
           {imagesArr.map((city, i) => (
-            <img src={city.img} className="thumb" key={i} onClick={() => handleClick(city.img)} />
+            <img src={city.img} className="thumb" key={i} onClick={() => handleClick(city.img, city.city)} />
           ))}
         </div>
       </div>
